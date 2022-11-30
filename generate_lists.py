9040dcb17f8b2e9f3f80b7cb6bceba1ca4511c70
artisan <artisan@amazon.com>
2022-11-30 15:17:37 -0500

    removed unused py files and added generate_subdomains.py

--- generate_lists.py
+++ generate_lists.py
@@ -8,25 +8,13 @@ def generate_lists():
     2. Subtracts latter from former
     3. Deduplicates output so only unique lines remain
     """
-    blacklisted_tlds, blacklisted_urls, blacklisted_ips = process_folder('blacklists')
-    whitelisted_tlds, whitelisted_urls, whitelisted_ips = process_folder('whitelists')
+    blacklisted_tlds = process_folder('blacklists')
+    whitelisted_tlds = process_folder('whitelists')
     final_tlds = list(set(blacklisted_tlds) - set(whitelisted_tlds))
-    final_urls = list(set(blacklisted_urls) - set(whitelisted_urls))
-    final_ips = list(set(blacklisted_ips) - set(whitelisted_ips))
     final_tlds.sort()
-    final_urls.sort()
-    final_ips.sort()
     with open("lists/tlds.txt", "w") as f:
         f.write("\n".join(final_tlds))
-    with open("lists/urls.txt", "w") as f:
-        f.write("\n".join(final_urls))
-    with open("lists/ips.txt", "w") as f:
-        f.write("\n".join(final_ips))
-    with open("lists/cidrs.txt", "w") as f:
-        f.write("/32\n".join(final_ips))
-        f.write("/32")
-    print(f'> DONE. Lists generated and contain {len(final_tlds)} TLDs, '
-          f'{len(final_urls)} URLs and {len(final_ips)} IPs.')
+    print(f'> DONE. Lists generated and contain {len(final_tlds)} TLDs.')
 
 
 if __name__ == '__main__':
--- refresh.sh
+++ refresh.sh
@@ -4,20 +4,9 @@ echo "==================================="
 pipenv run python generate_lists.py
 
 echo "==================================="
-echo "Generating IPs"
+echo "Generating list of subdomains"
 echo "==================================="
-pipenv run python generate_ips_from_lists.py
-
-# this step is necessary coz we now have updated IP lists from previous step that we want to include
-echo "==================================="
-echo "Re-generating lists with updated IPs"
-echo "==================================="
-pipenv run python generate_lists.py
-
-echo "==================================="
-echo "Generating hosts"
-echo "==================================="
-pipenv run python generate_hosts.py
+pipenv run python generate_subdomains.py
 
 echo "==================================="
 echo "ALL DONE. 🤘"
