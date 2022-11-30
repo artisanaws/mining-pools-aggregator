0cd3e4eca97bea40685837dbcded9350ed83fefc
artisan <artisan@amazon.com>
2022-11-30 16:26:50 -0500

    edit refresh.sh to split results into domain lists

--- refresh.sh
+++ refresh.sh
@@ -9,5 +9,16 @@ echo "==================================="
 pipenv run python generate_subdomains.py
 
 echo "==================================="
+echo "Formating Route 53 Resolver DNS Firewall domain lists"
+echo "==================================="
+split -l 1024 -d ./lists/tlds.txt ./R53domainlists/TldDomainList 
+split -l 1024 -d ./lists/subdomains.txt ./R53domainlists/SubdomainDomainList
+rename 's/$/.txt/' ./R53domainlists/*
+
+echo "==================================="
+echo "Domain lists are ready to be uploaded to S3"
+echo "==================================="
+
+echo "==================================="
 echo "ALL DONE. 🤘"
 echo "==================================="
